c3cdd9bd6bd2ba926aa9637d3fc13345af468ac4
MetovAidamir <mnvp@inbox.ru>
2021-12-20 03:38:45 +0300

    comm
    csv

--- Russian Robotics/UploadToDatabase.cs
+++ Russian Robotics/UploadToDatabase.cs
@@ -1,5 +1,10 @@
-﻿using Russian_Robotics.Models;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using Russian_Robotics.Models;
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 
@@ -7,52 +12,74 @@ namespace Russian_Robotics
 {
     class UploadToDatabase
     {
-        public void Upload(string fileName) { 
-        var readcsv = File.ReadAllText("C:/Users/mnvp/Desktop/Russin Robotics/"+ fileName);
-        string[] csvfilerecord = readcsv.Split('\n');
-        int i = 0;
+        public void Upload(string fileName)
+        {
             using (context db = new context())
             {
                 PriceItems items;
-                try
+                var badRow = new List<string>();
+                var config = new CsvConfiguration(CultureInfo.CurrentCulture)
                 {
-                    foreach (var row in csvfilerecord)
+
+                    Delimiter = ";",
+                    BadDataFound = arg => badRow.Add(arg.Context.Parser.RawRecord)
+                };
+                var reader = new StreamReader(@"C:/Users/mnvp/Desktop/Russin Robotics/"+ fileName);
+                var csvReader = new CsvReader(reader, config);
+                var reacords = csvReader.GetRecords<PriceitemsClassMap>().ToList();
+                int countRow = 1;
+                foreach (var row in reacords)
+                {
+                    countRow++;
+                    try
                     {
-                        if (!string.IsNullOrEmpty(row) && i >= 1)
+                        var searchNumber = new String(row.Number.Where(x => char.IsLetterOrDigit(x)
+                                          || char.IsWhiteSpace(x)).ToArray());
+                        var searchVendor = new String(row.Vendor.Where(x => char.IsLetterOrDigit(x)
+                                       || char.IsWhiteSpace(x)).ToArray());
+                        string count = "";
+                        if (row.Count[0] == '>' || row.Count[0] == '<')
                         {
-                            var cells = row.Split(';');
-                            var searchNumber=new String(cells[10].Where(x => char.IsLetterOrDigit(x)
-                                         || char.IsWhiteSpace(x)).ToArray());
-                            var searchVendor = new String(cells[1].Where(x => char.IsLetterOrDigit(x)
-                                           || char.IsWhiteSpace(x)).ToArray());
-                            string count="";
-                            foreach (char s in cells[8])
-                            {
-                                if(s=='>'||s=='<')
-                                {
-                                    count = new String(cells[8].Where(x => char.IsDigit(x)).ToArray());
-                                    break;
-                                }
-                                else
-                                {
-                                    count = cells[8].Substring(cells[8].IndexOf('-') + 1);
-                                }
-                            }
-                            items = new PriceItems { Vendor = cells[1], Number = cells[10], SearchNumber = searchNumber.ToUpper(), SearchVendor = searchVendor.ToUpper(), Description = cells[3], Price = Convert.ToDecimal(cells[6]), Count = Convert.ToInt32(count) };
-                            db.PriceItems.Add(items);
+                            count = row.Count.Substring(1);
+
                         }
                         else
                         {
-                            i++;
+                            count = row.Count.Substring(row.Count.IndexOf('-') + 1);
                         }
+                        items = new PriceItems { Vendor = row.Vendor, Number = row.Number, SearchNumber = searchNumber.ToUpper(), SearchVendor = searchVendor.ToUpper(), Description = row.Description, Price = Convert.ToDecimal(row.Price), Count = Convert.ToInt32(count) };
+                        db.PriceItems.Add(items);
+                        db.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message + " - Nomer: " + countRow);
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Возникло исключение: " + ex);
-                }
-                db.SaveChanges();
+                Console.WriteLine("Bad Date: " + badRow.Count);
             }
-        } 
+        }
+    }
+    class PriceitemsClassMap
+    {
+        [Name("Бренд")]
+        public string Vendor { get; set; }
+
+        [Name("Каталожный номер")]
+        public string Number { get; set; }
+
+        [Name("Бренд")]
+        public string SearchVendor { get; set; }
+
+        [Name("Каталожный номер")]
+        public string SearchNumber { get; set; }
+
+        [Name("Описание")]
+        public string Description { get; set; }
+
+        [Name("Цена, руб.")]
+        public string Price { get; set; }
+        [Name("Наличие")]
+        public string Count { get; set; }
     }
 }
